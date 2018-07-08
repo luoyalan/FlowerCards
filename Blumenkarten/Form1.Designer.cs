0ae400f138506795ba3fffd8b0b222b7ee17fd85
yll <yll@outlook.com>
2018-07-08 09:58:19 +0200

    initial commit

---
+++ Blumenkarten/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Blumenkarten
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.b1 = new System.Windows.Forms.PictureBox();
+            this.b2 = new System.Windows.Forms.PictureBox();
+            this.b3 = new System.Windows.Forms.PictureBox();
+            this.b4 = new System.Windows.Forms.PictureBox();
+            this.backside4 = new System.Windows.Forms.Button();
+            this.cover = new System.Windows.Forms.Button();
+            this.backside3 = new System.Windows.Forms.Button();
+            this.backside2 = new System.Windows.Forms.Button();
+            this.backside1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.b5 = new System.Windows.Forms.PictureBox();
+            this.b9 = new System.Windows.Forms.PictureBox();
+            this.b6 = new System.Windows.Forms.PictureBox();
+            this.b7 = new System.Windows.Forms.PictureBox();
+            this.b8 = new System.Windows.Forms.PictureBox();
+            this.backside5 = new System.Windows.Forms.Button();
+            this.backside6 = new System.Windows.Forms.Button();
+            this.backside7 = new System.Windows.Forms.Button();
+            this.backside8 = new System.Windows.Forms.Button();
+            this.backside9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.b1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Image = ((System.Drawing.Image)(resources.GetObject("b1.Image")));
+            this.b1.Location = new System.Drawing.Point(104, 28);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(106, 150);
+            this.b1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b1.TabIndex = 0;
+            this.b1.TabStop = false;
+            // 
+            // b2
+            // 
+            this.b2.Image = ((System.Drawing.Image)(resources.GetObject("b2.Image")));
+            this.b2.Location = new System.Drawing.Point(216, 28);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(106, 150);
+            this.b2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b2.TabIndex = 1;
+            this.b2.TabStop = false;
+            // 
+            // b3
+            // 
+            this.b3.Image = ((System.Drawing.Image)(resources.GetObject("b3.Image")));
+            this.b3.Location = new System.Drawing.Point(104, 188);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(106, 150);
+            this.b3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b3.TabIndex = 2;
+            this.b3.TabStop = false;
+            // 
+            // b4
+            // 
+            this.b4.Image = ((System.Drawing.Image)(resources.GetObject("b4.Image")));
+            this.b4.Location = new System.Drawing.Point(216, 188);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(106, 150);
+            this.b4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b4.TabIndex = 3;
+            this.b4.TabStop = false;
+            // 
+            // backside4
+            // 
+            this.backside4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside4.Location = new System.Drawing.Point(651, 327);
+            this.backside4.Name = "backside4";
+            this.backside4.Size = new System.Drawing.Size(106, 150);
+            this.backside4.TabIndex = 4;
+            this.backside4.Text = "?";
+            this.backside4.UseVisualStyleBackColor = true;
+            // 
+            // cover
+            // 
+            this.cover.Location = new System.Drawing.Point(651, 582);
+            this.cover.Name = "cover";
+            this.cover.Size = new System.Drawing.Size(113, 61);
+            this.cover.TabIndex = 5;
+            this.cover.Text = "cover";
+            this.cover.UseVisualStyleBackColor = true;
+            this.cover.Click += new System.EventHandler(this.cover_Click);
+            // 
+            // backside3
+            // 
+            this.backside3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside3.Location = new System.Drawing.Point(632, 276);
+            this.backside3.Name = "backside3";
+            this.backside3.Size = new System.Drawing.Size(106, 150);
+            this.backside3.TabIndex = 6;
+            this.backside3.Text = "?";
+            this.backside3.UseVisualStyleBackColor = true;
+            // 
+            // backside2
+            // 
+            this.backside2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside2.Location = new System.Drawing.Point(619, 249);
+            this.backside2.Name = "backside2";
+            this.backside2.Size = new System.Drawing.Size(106, 150);
+            this.backside2.TabIndex = 7;
+            this.backside2.Text = "?";
+            this.backside2.UseVisualStyleBackColor = true;
+            // 
+            // backside1
+            // 
+            this.backside1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside1.Location = new System.Drawing.Point(603, 214);
+            this.backside1.Name = "backside1";
+            this.backside1.Size = new System.Drawing.Size(106, 150);
+            this.backside1.TabIndex = 8;
+            this.backside1.Text = "?";
+            this.backside1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // b5
+            // 
+            this.b5.Image = ((System.Drawing.Image)(resources.GetObject("b5.Image")));
+            this.b5.Location = new System.Drawing.Point(328, 28);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(106, 150);
+            this.b5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b5.TabIndex = 9;
+            this.b5.TabStop = false;
+            // 
+            // b9
+            // 
+            this.b9.Image = ((System.Drawing.Image)(resources.GetObject("b9.Image")));
+            this.b9.Location = new System.Drawing.Point(328, 344);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(106, 150);
+            this.b9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b9.TabIndex = 10;
+            this.b9.TabStop = false;
+            // 
+            // b6
+            // 
+            this.b6.Image = ((System.Drawing.Image)(resources.GetObject("b6.Image")));
+            this.b6.Location = new System.Drawing.Point(328, 188);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(106, 150);
+            this.b6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b6.TabIndex = 11;
+            this.b6.TabStop = false;
+            // 
+            // b7
+            // 
+            this.b7.Image = ((System.Drawing.Image)(resources.GetObject("b7.Image")));
+            this.b7.Location = new System.Drawing.Point(104, 344);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(106, 150);
+            this.b7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b7.TabIndex = 12;
+            this.b7.TabStop = false;
+            // 
+            // b8
+            // 
+            this.b8.Image = ((System.Drawing.Image)(resources.GetObject("b8.Image")));
+            this.b8.Location = new System.Drawing.Point(216, 344);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(106, 150);
+            this.b8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.b8.TabIndex = 13;
+            this.b8.TabStop = false;
+            // 
+            // backside5
+            // 
+            this.backside5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside5.Location = new System.Drawing.Point(763, 214);
+            this.backside5.Name = "backside5";
+            this.backside5.Size = new System.Drawing.Size(106, 150);
+            this.backside5.TabIndex = 14;
+            this.backside5.Text = "?";
+            this.backside5.UseVisualStyleBackColor = true;
+            // 
+            // backside6
+            // 
+            this.backside6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside6.Location = new System.Drawing.Point(785, 249);
+            this.backside6.Name = "backside6";
+            this.backside6.Size = new System.Drawing.Size(106, 150);
+            this.backside6.TabIndex = 15;
+            this.backside6.Text = "?";
+            this.backside6.UseVisualStyleBackColor = true;
+            // 
+            // backside7
+            // 
+            this.backside7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside7.Location = new System.Drawing.Point(794, 276);
+            this.backside7.Name = "backside7";
+            this.backside7.Size = new System.Drawing.Size(106, 150);
+            this.backside7.TabIndex = 16;
+            this.backside7.Text = "?";
+            this.backside7.UseVisualStyleBackColor = true;
+            // 
+            // backside8
+            // 
+            this.backside8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside8.Location = new System.Drawing.Point(806, 327);
+            this.backside8.Name = "backside8";
+            this.backside8.Size = new System.Drawing.Size(106, 150);
+            this.backside8.TabIndex = 17;
+            this.backside8.Text = "?";
+            this.backside8.UseVisualStyleBackColor = true;
+            // 
+            // backside9
+            // 
+            this.backside9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backside9.Location = new System.Drawing.Point(819, 344);
+            this.backside9.Name = "backside9";
+            this.backside9.Size = new System.Drawing.Size(106, 150);
+            this.backside9.TabIndex = 18;
+            this.backside9.Text = "?";
+            this.backside9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 717);
+            this.Controls.Add(this.backside9);
+            this.Controls.Add(this.backside8);
+            this.Controls.Add(this.backside7);
+            this.Controls.Add(this.backside6);
+            this.Controls.Add(this.backside5);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.backside1);
+            this.Controls.Add(this.backside2);
+            this.Controls.Add(this.backside3);
+            this.Controls.Add(this.cover);
+            this.Controls.Add(this.backside4);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.b1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b8)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox b1;
+        private System.Windows.Forms.PictureBox b2;
+        private System.Windows.Forms.PictureBox b3;
+        private System.Windows.Forms.PictureBox b4;
+        private System.Windows.Forms.Button backside4;
+        private System.Windows.Forms.Button cover;
+        private System.Windows.Forms.Button backside3;
+        private System.Windows.Forms.Button backside2;
+        private System.Windows.Forms.Button backside1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox b5;
+        private System.Windows.Forms.PictureBox b9;
+        private System.Windows.Forms.PictureBox b6;
+        private System.Windows.Forms.PictureBox b7;
+        private System.Windows.Forms.PictureBox b8;
+        private System.Windows.Forms.Button backside5;
+        private System.Windows.Forms.Button backside6;
+        private System.Windows.Forms.Button backside7;
+        private System.Windows.Forms.Button backside8;
+        private System.Windows.Forms.Button backside9;
+    }
+}
+
---
+++ Blumenkarten/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blumenkarten
+{
+    public partial class Form1 : Form
+    {
+        public List<Control> pictures = new List<Control>();
+        public List<Control> backsides = new List<Control>();
+        private Point backsideoldlocation;
+        private Control gonebackside;
+        int visibleCardCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pictures.Add(b1);
+            pictures.Add(b2);
+            pictures.Add(b3);
+            pictures.Add(b4);
+            pictures.Add(b5);
+            pictures.Add(b6);
+            pictures.Add(b7);
+            pictures.Add(b8);
+            pictures.Add(b9);
+            backsides.Add(backside1);
+            backsides.Add(backside2);
+            backsides.Add(backside3);
+            backsides.Add(backside4);
+            backsides.Add(backside5);
+            backsides.Add(backside6);
+            backsides.Add(backside7);
+            backsides.Add(backside8);
+            backsides.Add(backside9);
+
+            for (int i = 0; i < pictures.Count; i++)
+            {
+             backsides[i].Click +=  backsideGoAway;
+            }
+
+
+        }
+        private void cover_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                backsides[i].Location = pictures[i].Location;
+
+            }
+
+        }
+
+        void backsideGoAway(object sender, EventArgs e)
+        {
+            visibleCardCount += 1;
+            timer1.Stop();
+            backsideGoback();
+            timer1.Start();
+            Control backside = (Control) sender;
+            gonebackside = backside;
+            backsideoldlocation = new Point(backside.Location.X, backside.Location.Y);
+            backside.Location = new Point(603, 214);
+
+        } 
+
+        void backsideGoback()
+        {  
+            if ( gonebackside != null)
+            {
+                gonebackside.Location = backsideoldlocation;
+            }
+
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                backsideGoback();
+                timer1.Stop();
+        }
+    }
+}
